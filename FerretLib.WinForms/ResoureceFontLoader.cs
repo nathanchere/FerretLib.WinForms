4138c3b4095e08f1e5a0774ab2026ea8eb297f8f
Nathan Chere <git@nathanchere.com.au>
2014-01-23 15:28:18 +1100

    Trying to fix AccessViolationException bug with PrivateFontCollection

--- FerretLib.WinForms/ResoureceFontLoader.cs
+++ FerretLib.WinForms/ResoureceFontLoader.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Text;
 using System.IO;
@@ -9,25 +8,27 @@ using System.Text;
 
 namespace FerretLib.WinForms
 {
-    public class ResourceFont
+
+    public class ResourceFont : IDisposable
     {
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, uint cbFont, IntPtr pdv, [In] ref uint pcFonts);
+
         private PrivateFontCollection _font;
+
+        private IntPtr _fontPointer;
+        private GCHandle _handle;
 
         public ResourceFont(byte[] rawFont)
         {
-            System.IntPtr data = new IntPtr();
-            try
+            _font = new PrivateFontCollection();
+            using (var stream = new MemoryStream(rawFont))
             {
-                _font = new PrivateFontCollection();
-                using (var stream = new MemoryStream(rawFont)) {
-                    data = Marshal.AllocCoTaskMem((int)stream.Length);
-                    Marshal.Copy(rawFont, 0, data, (int)stream.Length);
-                    _font.AddMemoryFont(data, (int)stream.Length);
-                }                
-            }
-            finally
-            {
-                Marshal.FreeCoTaskMem(data);
+                _handle = GCHandle.Alloc(rawFont, GCHandleType.Pinned);
+                _fontPointer = _handle.AddrOfPinnedObject();
+                _font.AddMemoryFont(_fontPointer, (int)stream.Length);
+                uint dummy = 0;
+                AddFontMemResourceEx(_fontPointer, (uint)stream.Length, IntPtr.Zero, ref dummy);                
             }
         }
 
@@ -36,5 +37,41 @@ namespace FerretLib.WinForms
             if (_font == null || _font.Families.Length == 0) throw new InvalidDataException("No font data loaded");
             return new Font(_font.Families[0], ems);
         }
+
+        public void Dispose()
+        {
+            if(_handle != null) _handle.Free();
+            if (_font != null) _font.Dispose();
+        }
     }
+
+    //public class ResourceFont
+    //{
+    //    [DllImport("gdi32.dll")]
+    //    private static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, uint cbFont, IntPtr pdv, [In] ref uint pcFonts);
+
+    //    private PrivateFontCollection _font;
+
+    //    public ResourceFont(byte[] rawFont)
+    //    {
+    //        _font = new PrivateFontCollection();
+    //        using (var stream = new MemoryStream(rawFont))
+    //        {
+    //            var pinnedArray = GCHandle.Alloc(rawFont, GCHandleType.Normal);
+    //            var pointer = pinnedArray.AddrOfPinnedObject();
+    //            _font.AddMemoryFont(pointer, (int)stream.Length);
+    //            uint dummy = 0;
+    //            AddFontMemResourceEx((IntPtr)pointer, (uint)stream.Length, IntPtr.Zero, ref dummy);
+    //            pinnedArray.Free();
+    //        }
+    //    }
+
+    //    public Font GetFont(float ems)
+    //    {
+    //        if (_font == null || _font.Families.Length == 0) throw new InvalidDataException("No font data loaded");
+    //        return new Font(_font.Families[0], ems);
+    //    }
+    //}
+
+
 }
