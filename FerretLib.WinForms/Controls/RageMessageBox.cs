4e896cf0e793cfc702585347a70e08dd16fbdf69
Nathan Chere <nathan.chere@tretton37.com>
2015-01-13 11:44:33 +0100

    init buttons

--- FerretLib.WinForms/Controls/RageMessageBox.cs
+++ FerretLib.WinForms/Controls/RageMessageBox.cs
@@ -11,9 +11,93 @@ namespace FerretLib.WinForms.Controls
 {
     public partial class RageMessageBox : Form
     {
-        public RageMessageBox()
+        public DialogResult Result { get; private set; }
+
+        public enum RageMessageBoxButtons
+        {
+        }
+
+        public enum RageMessageBoxDefaultButton
+        {
+            OK,
+            OKCancel,
+            RetryCancel,
+            YesNo,
+            YesNoCancel,
+            AbortRetryIgnore,
+            FFFFFUUUUUUUU,
+            LOL,            
+            Derp,
+        }
+
+        public enum RageMessageBoxIcon
+        {
+            None,
+            FFFFFUUUUUUUU,
+            SuperRage,
+            Derp,
+            Trolling,
+            Facepalm,
+            EpicWin,
+            YUNo,
+        }
+
+        public RageMessageBox(string text, RageMessageBoxDefaultButton buttons, RageMessageBoxIcon icon)
         {
             InitializeComponent();
+            InitializeButtons(buttons);
+            InitializeDialogLayout();
+        }
+
+        private void InitializeButtons(RageMessageBoxDefaultButton buttons)
+        {
+            if (buttons == RageMessageBoxDefaultButton.OK || buttons == RageMessageBoxDefaultButton.OKCancel)
+                AddButton("OK", DialogResult.OK);
+
+            if (buttons == RageMessageBoxDefaultButton.AbortRetryIgnore)
+                AddButton("Abort", DialogResult.Abort);
+
+            if (buttons == RageMessageBoxDefaultButton.AbortRetryIgnore || buttons == RageMessageBoxDefaultButton.RetryCancel)
+                AddButton("Retry", DialogResult.Retry);
+
+            if (buttons == RageMessageBoxDefaultButton.YesNo || buttons == RageMessageBoxDefaultButton.YesNoCancel)
+            {
+                AddButton("Yes", DialogResult.Yes);
+                AddButton("No", DialogResult.No);
+            }
+
+            if (buttons == RageMessageBoxDefaultButton.RetryCancel || buttons == RageMessageBoxDefaultButton.OKCancel || buttons == RageMessageBoxDefaultButton.YesNoCancel)
+                AddButton("Cancel", DialogResult.Cancel);
+
+            if (buttons == RageMessageBoxDefaultButton.AbortRetryIgnore)
+                AddButton("Ignore", DialogResult.Ignore);
+
+            if (buttons == RageMessageBoxDefaultButton.FFFFFUUUUUUUU || buttons == RageMessageBoxDefaultButton.Derp || buttons == RageMessageBoxDefaultButton.LOL)
+                AddButton(buttons.ToString(), DialogResult.OK);
+        }
+
+        private void AddButton(string caption, DialogResult result)
+        {
+            var button = new Button
+            {
+                Text = caption,
+                Width = 70,
+                Height = 33,
+            };
+
+            var parent = this;
+            button.Click += (sender, args) =>
+            {
+                parent.Result = result;
+                parent.Close();
+            };
+
+            panelButtons.Controls.Add(button);
+        }
+
+        private void RageMessageBox_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
